a3ba97165b175f3d39c34c3bb9e4ac4e68361582
chetan6290 <chetangaikwad6290@gmail.com>
2026-05-09 23:53:39 +0530

    updated bot polling

--- bot.py
+++ bot.py
@@ -11,15 +11,30 @@ from handlers.start import start
 from handlers.notes import get_notes
 from handlers.upload import upload_handler
 
+
+# -----------------------------------
+# CREATE BOT APPLICATION
+# -----------------------------------
 app = Application.builder().token(TOKEN).build()
 
-# Start
-app.add_handler(CommandHandler("start", start))
 
-# Upload System
+# -----------------------------------
+# START COMMAND
+# -----------------------------------
+app.add_handler(
+    CommandHandler("start", start)
+)
+
+
+# -----------------------------------
+# ADMIN FILE UPLOAD
+# -----------------------------------
 app.add_handler(upload_handler)
 
-# Search Notes
+
+# -----------------------------------
+# SEARCH NOTES / QUESTION PAPERS
+# -----------------------------------
 app.add_handler(
     MessageHandler(
         filters.TEXT & ~filters.COMMAND,
@@ -27,6 +42,14 @@ app.add_handler(
     )
 )
 
+
+# -----------------------------------
+# RUN BOT
+# -----------------------------------
+print("MongoDB Connected Successfully")
 print("Bot Running...")
 
-app.run_polling()
+
+app.run_polling(
+    drop_pending_updates=True
+)
